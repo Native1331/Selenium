1381aa945cb7b0c54272cea1d709f60b2b43457c
Natalya Kadysheva <83497921+Native1331@users.noreply.github.com>
2022-05-28 11:07:13 +0300

    Create AuthConfig.java

---
+++ src/test/java/io/qaguru/owner/config/AuthConfig.java
@@ -0,0 +1,15 @@
+package io.qaguru.owner.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "file:/tmp/secret.properties",
+        "classpath:auth.properties"
+})
+public interface AuthConfig extends Config {
+
+    @Key("username")
+    String username();
+
+    @Key("password")
+    String password();
