8ccffc18a4d9cbc5dd45c397bcba4344aeb7169e
Natalya Kadysheva <83497921+Native1331@users.noreply.github.com>
2022-05-28 11:06:17 +0300

    Create AndroidConfig.java

---
+++ src/test/java/io/qaguru/owner/config/AndroidConfig.java
@@ -0,0 +1,19 @@
+package io.qaguru.owner.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:pixel.properties"
+})
+public interface AndroidConfig extends Config {
+
+    @Key("platform.name")
+    String platformName();
+
+    @Key("platform.version")
+    String platformVersion();
+
+    @Key("device.name")
+    String deviceName();
+
+}
